55691a78493b17a722f6416971a25a45b83eea2d
tundeiness <tundeiness@yahoo.co.uk>
2021-11-01 16:07:43 +0100

    add col systems

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 const App = () => (
-  <div className="App container">
+  <div className="App container border-blue">
     <p>Hello world</p>
+    {/* <div className="col-12 border-red">1</div>
+    <div className="col-10 border-red">1</div>
+    <div className="col-2 border-red">1</div> */}
+    <div className="row">
+      {/* <div className="col-1 border-red">1</div>
+      <div className="col-1 border-red">1</div>
+      <div className="col-1 border-red">1</div>
+      <div className="col-1 border-red">1</div>
+      <div className="col-1 border-red">1</div>
+      <div className="col-1 border-red">1</div> */}
+      <div className="col-11 border-red">1</div>
+      <div className="col-1 border-red">1</div>
+      {/* <div className="col-4 border-red">1</div> */}
+      {/* <div className="col-1 border-red">1</div>
+      <div className="col-1 border-red">1</div> */}
+      {/* <div className="col-3 border-red">1</div> */}
+      {/* <div className="col-2 border-red">1</div>
+      <div className="col-2 border-red">1</div> */}
+      {/* <div className="col-4 border-red">1</div> */}
+      {/* <div className="col-3 border-red">1</div> */}
+      {/* <div className="col-2 border-red">1</div>
+      <div className="col-2 border-red">1</div>
+      <div className="col-2 border-red">1</div>
+      <div className="col-2 border-red">1</div> */}
+    </div>
   </div>
 );
 
