a3f067ddb6be75c2c9eb254c0af7c9e5ebb64619
tundeiness <tundeiness@yahoo.co.uk>
2021-11-08 20:14:41 +0100

    w.i.p sidebar mobile

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,6 +6,7 @@ import Logo from 'static/assets/image/shared/logo.svg';
 
 const Header = () => {
   const [isActive, setIsActive] = useState('Home');
+  const [isSideBar, setIsSideBar] = useState(false);
   const [show, setShow] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
@@ -14,22 +15,87 @@ const Header = () => {
   };
 
   return (
-    <header className="pry-header">
-      <div className="pry-header__img-section border-red">
-        <div className="img-wrap">
-          <img src={Logo} alt="space tourism logo" className="logo" />
+    <>
+      <header className="pry-header">
+        <div className="pry-header__img-section border-red">
+          <picture className="img-wrap">
+            <source srcSet={Logo} media="prefers-color-scheme: dark" />
+            <img
+              src={Logo}
+              alt="space tourism logo"
+              className="logo"
+              loading="lazy"
+            />
+          </picture>
         </div>
-      </div>
-      <button
-        type="button"
-        className="mobile-nav-toggle"
-        aria-controls="primary-navigation"
-      >
-        <span className="sr-mobile" aria-expanded="false">
-          Menu
-        </span>
-      </button>
-      <nav className="pry-header__nav">
+        <button
+          type="button"
+          className="mobile-nav-toggle"
+          aria-controls="primary-navigation"
+        >
+          <span className="sr-mobile" aria-expanded="false" />
+          {/* </span> */}
+        </button>
+        <nav className="pry-header__nav">
+          <ul id="pry-navigation" className="pry-navigation">
+            <li className={isActive === 'Home' ? 'active' : 'inactive'}>
+              <a
+                className="ff-sans-cond uppercase text-white letter-spacing-2"
+                href="index.html"
+              >
+                <span>00</span>
+                Home
+              </a>
+              {' '}
+            </li>
+
+            <li
+              className={isActive === 'Destination' ? 'active' : 'inactive'}
+              onClick={() => handleClick('Destination')}
+              aria-hidden="true"
+            >
+              <a
+                className="ff-sans-cond uppercase text-white letter-spacing-2"
+                href="destination.html"
+              >
+                <span>01</span>
+                Destination
+              </a>
+              {' '}
+            </li>
+
+            <li
+              className={isActive === 'Crew' ? 'active' : 'inactive'}
+              onClick={() => handleClick('Crew')}
+              aria-hidden="true"
+            >
+              <a
+                className="ff-sans-cond uppercase text-white letter-spacing-2"
+                href="crew.html"
+              >
+                <span>02</span>
+                Crew
+              </a>
+              {' '}
+            </li>
+
+            <li
+              className={isActive === 'Technology' ? 'active' : 'inactive'}
+              onClick={() => handleClick('Technology')}
+              aria-hidden="true"
+            >
+              <a
+                className="ff-sans-cond uppercase text-white letter-spacing-2"
+                href="technology.html"
+              >
+                <span>03</span>
+                Technology
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="sidebar">
         <ul id="pry-navigation" className="pry-navigation">
           <li className={isActive === 'Home' ? 'active' : 'inactive'}>
             <a
@@ -86,8 +152,8 @@ const Header = () => {
             </a>
           </li>
         </ul>
-      </nav>
-    </header>
+      </div>
+    </>
   );
 };
 
---
+++ src/view/home/home.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Home = () => {
+  const test = 1;
+  return (
+    <div className="home">
+      <p>Hello home</p>
+    </div>
+  );
+};
+
+export default Home;
