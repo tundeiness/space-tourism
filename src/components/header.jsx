6b81f88daf3bee740b0f624617e4c92e25e292a6
tundeiness <tundeiness@yahoo.co.uk>
2021-11-12 11:43:43 +0100

    clean up header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import Logo from 'static/assets/image/shared/logo.svg';
 import Sidebar from 'components/sideBar';
 import useToggle from 'utils/toggle';
-// import 'static/sass/components/global.scss';
 
 const Header = () => {
   const [isActive, setIsActive] = useState('Home');
@@ -16,11 +15,6 @@ const Header = () => {
     setIsActive(link);
   };
 
-  // const handleSideBar = () => {
-  //   setIsSideBar(true);
-  // };
-
-  useEffect(() => console.log('UseEffect says:', isSideBar));
   return (
     <>
       <header className="pry-header">
@@ -103,64 +97,6 @@ const Header = () => {
         </nav>
       </header>
       {isSideBar ? '' : <Sidebar check={isSideBar} />}
-      {/* <div className="sidebar">
-        <ul id="pry-navigation" className="pry-navigation">
-          <li className={isActive === 'Home' ? 'active' : 'inactive'}>
-            <a
-              className="ff-sans-cond uppercase text-white letter-spacing-2"
-              href="index.html"
-            >
-              <span>00</span>
-              Home
-            </a>
-            {' '}
-          </li>
-
-          <li
-            className={isActive === 'Destination' ? 'active' : 'inactive'}
-            onClick={() => handleClick('Destination')}
-            aria-hidden="true"
-          >
-            <a
-              className="ff-sans-cond uppercase text-white letter-spacing-2"
-              href="destination.html"
-            >
-              <span>01</span>
-              Destination
-            </a>
-            {' '}
-          </li>
-
-          <li
-            className={isActive === 'Crew' ? 'active' : 'inactive'}
-            onClick={() => handleClick('Crew')}
-            aria-hidden="true"
-          >
-            <a
-              className="ff-sans-cond uppercase text-white letter-spacing-2"
-              href="crew.html"
-            >
-              <span>02</span>
-              Crew
-            </a>
-            {' '}
-          </li>
-
-          <li
-            className={isActive === 'Technology' ? 'active' : 'inactive'}
-            onClick={() => handleClick('Technology')}
-            aria-hidden="true"
-          >
-            <a
-              className="ff-sans-cond uppercase text-white letter-spacing-2"
-              href="technology.html"
-            >
-              <span>03</span>
-              Technology
-            </a>
-          </li>
-        </ul>
-      </div> */}
     </>
   );
 };
