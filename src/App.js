e25dd88c77478a6f75aae696a60056e69b99df27
tundeiness <tundeiness@yahoo.co.uk>
2021-11-13 17:44:14 +0100

    add background for desktop device

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import Header from 'components/header';
+// import Header from 'components/header';
+import Home from 'view/home/home';
 
 const App = () => (
   <div className="App container border-blue">
-    <Header />
+    {/* <Header /> */}
+    <Home />
   </div>
 );
 
--- src/view/home/home.jsx
+++ src/view/home/home.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import Header from 'components/header';
 
 const Home = () => {
   const test = 1;
   return (
     <div className="home">
-      <p>Hello home</p>
+      {/* <p>Hello home</p> */}
+      <Header />
     </div>
   );
 };
