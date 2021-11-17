fc9373b9ef7587d2fd4dbcdc28bd80c3057b0339
tundeiness <tundeiness@yahoo.co.uk>
2021-11-17 20:20:08 +0100

    clean up sideBar

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -2,31 +2,15 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 
-const SideBar = ({ check }) => {
+const SideBar = () => {
   const [isActive, setIsActive] = useState('Home');
-  const [isSideBar, setIsSideBar] = useState(false);
-  const [show, setShow] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = (link) => {
     setIsActive(link);
   };
 
-  const handleSideBar = () => {
-    setIsSideBar(true);
-  };
   return (
     <div className="sidebar">
-      {/* <div className={check ? 'close' : 'sidebar'}> */}
-      {/* <div className="sidebar__btn-wrap">
-        <button
-          type="button"
-          className={isSideBar === false ? 'mobile-nav-toggle' : 'open-mobile'}
-          aria-controls="primary-navigation"
-        >
-          <span className="sr-mobile" aria-expanded="false" />
-        </button>
-      </div> */}
       <ul id="side-nav" className="sidebar__side-nav">
         <li className={isActive === 'Home' ? 'side-active' : 'side-inactive'}>
           <a
