4a8d86d81936b576e3df4ceeae200e4a068da780
tundeiness <tundeiness@yahoo.co.uk>
2021-11-18 16:04:02 +0100

    setup home section

--- src/view/home/home.jsx
+++ src/view/home/home.jsx
@@ -5,9 +5,29 @@ import Header from 'components/header';
 const Home = () => {
   const test = 1;
   return (
-    <div className="home">
-      {/* <p>Hello home</p> */}
+    <div className="home container">
       <Header />
+      <section className="row body-section">
+        <div className="col-6 border-red body-section__message">
+          <div className="message-wrapper">
+            <h1 className="message-wrapper__heading text-accent fs-500 ff-sans-cond uppercase letter-spacing-1">
+              So, you want to travel to
+              <span className="d-block fs-900 ff-serif text-white">Space</span>
+            </h1>
+
+            <p>
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+              {' '}
+            </p>
+          </div>
+        </div>
+        <div className="col-6 border-red">
+          <div className="explore">Explore</div>
+        </div>
+      </section>
     </div>
   );
 };
